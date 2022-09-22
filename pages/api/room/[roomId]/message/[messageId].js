1494220e9c02bcfbd849eb87c28c69f1efe7ab07
NeZE <boss-NeZE@hotmail.com>
2022-09-22 15:19:39 +0700

    12

--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -4,4 +4,22 @@ export default function roomIdMessageIdRoute(req, res) {
   //read value from URL
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
+  if (req.method === "DELETE") {
+    const rooms = readDB();
+    const rommIDX = rooms.findIndex((x) => x.roomId === roomId);
+
+    if (rommIDX === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room ID" });
+    }
+    const textidx = rooms[rommIDX].messages.findIndex(
+      (x) => x.messageId === messageId
+    );
+    if (textidx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid message ID" });
+    }
+    rooms[rommIDX].messages.splice(textidx, 1);
+    writeDB(rooms);
+
+    return res.json({ ok: true });
+  }
 }
